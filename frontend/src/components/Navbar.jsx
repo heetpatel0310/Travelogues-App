18016469250f91fcfa2c5c7c52fb5831fcb2ed8e
Heet Patel <heetp0310@gmail.com>
2024-11-14 18:20:59 -0600

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({ userInfo, searchQuery, setSearchQuery, onSearchNote, handleCle
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
-      <img src={LOGO} alt="travel story" className="h-9" />
+      <img src={LOGO} alt="travelogues app" className="h-9" />
 
       {isToken && (
         <>
